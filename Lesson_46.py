dc9c2ade893dbf929e00cb3cba2cd9818cd46a02
EKSKA-201 <ardashirov11@mail.ru>
2025-08-31 11:38:23 +0500

    Update Lesson_46.py bez minusa

--- Lesson_46.py
+++ Lesson_46.py
@@ -3,8 +3,6 @@ vel_num_2 = float(input())
 c = str(input())
 if c == "+":
     print (vel_num_1+vel_num_2)
-elif c == "-":
-    print (vel_num_1-vel_num_2)
 elif c == "*":
     print (vel_num_1*vel_num_2)
 elif c == "/":
@@ -16,4 +14,5 @@ elif c == "//":
 elif c == "%":
     print (vel_num_1%vel_num_2)
 else:
+
     print("ERROR")
